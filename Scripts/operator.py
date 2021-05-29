925c20c7ad05698e531a7212d5cc1152b02d3868
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-05-29 22:59:28 +0300

    UPDATE: operator.py v2, added poll, props, etc

--- Scripts/operator.py
+++ Scripts/operator.py
@@ -4,12 +4,32 @@ class MESH_OT_object_creator(bpy.types.Operator):
     """Creates some object"""
     bl_idname = 'mesh.object_creator'
     bl_label = 'Object Creator'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    count_x = bpy.props.IntProperty(
+        name = "Count x",
+        description = "Amount of obj at X-dir",
+        default=1,
+        min = 1, soft_max = 10,
+    )
+
+    size = bpy.props.FloatProperty(
+        name = "Size",
+        description = "Size of objs",
+        default=1,
+        min = 0, soft_max = 2,
+    )
 
     def execute(self, context):
-        for adds in range(20):
-            bpy.ops.mesh.primitive_cube_add()
+        for adds in range(self.count_x):
+            bpy.ops.mesh.primitive_cube_add(location=(adds, 1, 1), size=self.size)
 
         return {'FINISHED'}
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.area.type == 'VIEW_3D'
+
 
 def menu_func(self, context):
     self.layout.operator(MESH_OT_object_creator.bl_idname)
