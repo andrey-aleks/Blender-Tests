75339500417d50a5500de053dc0e7b9d9f8abb46
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-06-13 23:49:26 +0300

    ADD: added simple custom prop script

--- Scripts/tests/custom_props_tests.py
+++ Scripts/tests/custom_props_tests.py
@@ -1,5 +1,17 @@
 import bpy
 
+class PropGroup(bpy.types.PropertyGroup):
+    my_int: bpy.props.IntProperty()
+    my_float: bpy.props.FloatProperty()
+
+def register():
+    bpy.utils.register_class(PropGroup)
+
+
+if __name__ == '__main__':
+    register()
+
+
 #class OT
     #bl
 
