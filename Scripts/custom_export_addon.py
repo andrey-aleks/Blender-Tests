ad4dd8a1d59f6c3a9b36854751b9570e15799b39
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-06-26 21:32:47 +0300

    FIX: refactored custom import/export addon

--- Scripts/custom_export_addon.py
+++ Scripts/custom_export_addon.py
@@ -1,68 +1,65 @@
 bl_info = {
-    "name": "Custom Im/Ex-port",
-    "version": (0, 1, 0),
+    "name": "Custom Import/Export",
+    "version": (1, 0, 0),
     "blender": (2, 80, 3),
-    "author": "Andrey <andrey.aleks00@gmail.com>",
+    "author": "Andrey Alekseev <andrey.aleks00@gmail.com>",
+    "location": "3D-viewport -> N-panel -> Tool -> Custom Import/Export",
+    "description": "Allows you to import some FBX, do some changes and rewrite this FBX at one click",
+    "warning": "Works only with FBX",
     "category": "Object",
 }
-import os
 import bpy
 import pathlib
 
-def import_files(context, import_fname):
-    abspath = bpy.path.abspath(context.scene.import_path)
-    import_path = pathlib.Path(abspath)        
-    for import_fpath in import_path.glob(str(import_fname)):
-        bpy.ops.import_scene.fbx(filepath=str(import_fpath))
-        for imported_fbx in context.selected_objects:
-            imported_fbx.import_fname = import_fpath.name
 
+# returns file's path
+def get_fname_path(context):
+        abspath = bpy.path.abspath(context.scene.fname)
+        fname_path = pathlib.Path(abspath)
+        return fname_path   
+
+
+# import operator
 class IMPORT_SCENE_OT_importer(bpy.types.Operator):
     bl_idname = 'import_scene.fbx_custom'
     bl_label = "Import FBX"
 
     def execute(self, context):
-
-        abspath = bpy.path.abspath(context.scene.export_fname)
-        export_path = pathlib.Path(abspath)        
-        bpy.ops.import_scene.fbx(filepath=str(export_path))
+        bpy.ops.import_scene.fbx(filepath=str(get_fname_path(context)))
         return {'FINISHED'}
 
 
+# export operator. Uses selected objects, applies all transforms, export selected
+# Export settings: "Selected Objects" true, Object Types - only MESH, "Apply Transform" true, "Bake Animations" false. Other settings by default
 class EXPORT_SCENE_OT_custom_export(bpy.types.Operator):
     bl_idname = 'export_scene.fbx_custom'
     bl_label = 'Export FBX'
 
     def execute(self, context):
-
         export_obs = context.selected_objects
         if export_obs:
             bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-            abspath = bpy.path.abspath(context.scene.export_fname)
-            export_path = pathlib.Path(abspath) 
-            bpy.ops.export_scene.fbx(filepath=str(export_path), use_selection=True, object_types={'MESH'}, bake_space_transform=True, bake_anim=False)
+            bpy.ops.export_scene.fbx(filepath=str(get_fname_path(context)), use_selection=True, object_types={'MESH'}, bake_space_transform=True, bake_anim=False)
             return {'FINISHED'}
         else:
             return {'CANCELLED'}
 
 
-
-
-
+# UI panel
 class VIEW3D_PT_custom_export(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Tools'
-    bl_label = 'Im/Ex'
+    bl_category = 'Tool'
+    bl_label = 'Custom Import/Export'
 
     def draw(self, context):
         col = self.layout
-        col.prop(context.scene, 'export_fname')
+        col.prop(context.scene, 'fname')
         col.operator('import_scene.fbx_custom')
         col = self.layout
         col.operator('export_scene.fbx_custom')
 
-
+##
 
 blender_classes = [
     VIEW3D_PT_custom_export,
@@ -70,17 +67,10 @@ blender_classes = [
     EXPORT_SCENE_OT_custom_export,
 ]
 
-
 ## 
 
 def register():
-    bpy.types.Scene.export_path = bpy.props.StringProperty(
-        name = "FBX Folder",
-        subtype='DIR_PATH',
-        default='//',
-    )
-
-    bpy.types.Scene.export_fname = bpy.props.StringProperty(
+    bpy.types.Scene.fname = bpy.props.StringProperty(
         name = "FBX Filename",
         subtype='FILE_PATH',
     )
@@ -89,8 +79,7 @@ def register():
         bpy.utils.register_class(blender_class)
 
 def unregister():
-    del bpy.types.Scene.export_path
-    del bpy.types.Scene.export_fname
+    del bpy.types.Scene.fname
 
     for blender_class in blender_classes:
         bpy.utils.unregister_class(blender_class)
