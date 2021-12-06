54f2995f2c70347aa9c6f798286392616668599b
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-12-06 17:11:49 +0300

    FIX: added force_set prop for bones contraints autosetter

--- Scripts/adds/autorig_setter_addon.py
+++ Scripts/adds/autorig_setter_addon.py
@@ -9,9 +9,21 @@ bl_info = {
 }
 import bpy
 
+class AddonSettings(bpy.types.PropertyGroup):
+    force_set: bpy.props.BoolProperty(
+        name="Force Set", 
+        default=False,
+        description="If true, tool will set all Targets to Source Rig Object. If false, tool will set only empty Targets"
+    )   
+    source_rig: bpy.props.PointerProperty(
+        name="Source Rig Object", 
+        type=bpy.types.Object,
+    )
+
+
 # Main operator 
-class Bones_Constraints_AutoSetter_OT(bpy.types.Operator):
-    bl_idname = 'mesh.bones_constraints_autosetter'
+class BONE_OT_Bones_Constraints_AutoSetter(bpy.types.Operator):
+    bl_idname = 'bone.bones_constraints_autosetter'
     bl_label = 'BonesConstraintsAutoSetter'
     bl_description = "Set Source Rig as \"Target\" for bones constraints"
     bl_options = {'REGISTER', 'UNDO'}
@@ -26,10 +38,12 @@ class Bones_Constraints_AutoSetter_OT(bpy.types.Operator):
 
     def execute(self, context):
         obj = bpy.context.active_object
+        settings = context.scene.addon_settings
         for bone in obj.pose.bones:
             for constraint in bone.constraints:                
                 if any(constraint.type in _constraint for _constraint in self.constraints):
-                    constraint.target = context.scene.source_rig
+                    if constraint.target is None or settings.force_set:
+                        constraint.target = settings.source_rig
 
         return {'FINISHED'}
 
@@ -44,16 +58,18 @@ class VIEW3D_PT_Bones_Constraints_AutoSetter(bpy.types.Panel):
     def draw(self, context):
         col = self.layout.column()
 
-        col.prop(context.scene, "source_rig")
-        col.operator('mesh.bones_constraints_autosetter', text="Set \"Target\"")
+        col.prop(context.scene.addon_settings, "source_rig")
+        col.prop(context.scene.addon_settings, "force_set")
+        col.operator('bone.bones_constraints_autosetter', text="Set \"Target\"")
 
         
 
 ##
 
 blender_classes = [
-    Bones_Constraints_AutoSetter_OT,
+    BONE_OT_Bones_Constraints_AutoSetter,
     VIEW3D_PT_Bones_Constraints_AutoSetter,
+    AddonSettings,
 ]
 
 ## 
@@ -61,10 +77,10 @@ blender_classes = [
 def register():
     for blender_class in blender_classes:
         bpy.utils.register_class(blender_class)
-    bpy.types.Scene.source_rig = bpy.props.PointerProperty(type=bpy.types.Object, name="Source Rig Object")
+    bpy.types.Scene.addon_settings = bpy.props.PointerProperty(type=AddonSettings)
 
 
 def unregister():
     for blender_class in blender_classes:
         bpy.utils.unregister_class(blender_class)
-    del bpy.types.Scene.source_rig
+    del bpy.types.Scene.addon_settings
